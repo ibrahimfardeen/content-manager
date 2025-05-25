287b58cfdca9047af1c900c6cfca9ab44240fc6f
Premkumar <118468266+Premkumar708@users.noreply.github.com>
2025-05-25 12:59:46 +0530

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -6,10 +6,10 @@ const getCorsHeaders = (origin: string) => {
   const headers = {
     "Access-Control-Allow-Methods": process.env.ALLOWED_METHODS,
     "Access-Control-Allow-Headers": process.env.ALLOWED_HEADERS,
-    "Access-Control-Allow-Origin": ""
+    "Access-Control-Allow-Origin": "*",
   };
   if (process.env.DOMAIN_URL.includes(origin)) {
-    headers["Access-Control-Allow-Origin"] = origin; 
+    headers["Access-Control-Allow-Origin"] = origin;
   }
 
   return headers;
@@ -36,53 +36,82 @@ export async function POST(req) {
     // console.log(JSON.stringify(data));
     // console.log(req.header.origin)
     return NextResponse.json({
-      msg: ["Data fetched successfully"],
+      msg: ["Data fetched successfully.."],
       success: true,
-      //data: data,
       data: responseObj(data),
     });
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ msg: ["Unable to get message."] });
+    return NextResponse.json({ msg: ["Unable to get message.."] });
   }
 
-  
   function responseObj(products) {
     // implement resource here
     var result = {
-      data: [],
+      
+      data: {}
     };
 
     for (var i in products) {
-      var dataObj = {
-        id: products[i]._id,
-        attributes: {
-          Name: products[i].name,
-          Price: products[i].price,
-          Desc: products[i].description,
-          createdAt: products[i].created,
-          updatedAt: products[i].updated,
-          publishedAt: products[i].created,
-          img: {
-            data: {
-              id: products[i]._id,
+      if(!result.data.hasOwnProperty(products[i].category)){
+          result.data[products[i].category] = {
+            data: [{id: products[i]._id,
               attributes: {
-                alternativeText: products[i].name,
-                formats: {
-                  thumbnail: {
-                    url: products[i].photo,
-                  },
-                },
-                url: products[i].photo,
+                Name: products[i].name,
+                Price: products[i].price,
+                Desc: products[i].description,
                 createdAt: products[i].created,
                 updatedAt: products[i].updated,
+                publishedAt: products[i].created,
+                img: {
+                  data: {
+                    id: products[i]._id,
+                    attributes: {
+                      alternativeText: products[i].name,
+                      formats: {
+                        thumbnail: {
+                          url: products[i].photo,
+                        },
+                      },
+                      url: products[i].photo,
+                      createdAt: products[i].created,
+                      updatedAt: products[i].updated,
+                    },
+                  },
+                },
+              },}]
+          }
+      }else{
+        result.data[products[i].category].data.push({
+          id: products[i]._id,
+              attributes: {
+                Name: products[i].name,
+                Price: products[i].price,
+                Desc: products[i].description,
+                createdAt: products[i].created,
+                updatedAt: products[i].updated,
+                publishedAt: products[i].created,
+                img: {
+                  data: {
+                    id: products[i]._id,
+                    attributes: {
+                      alternativeText: products[i].name,
+                      formats: {
+                        thumbnail: {
+                          url: products[i].photo,
+                        },
+                      },
+                      url: products[i].photo,
+                      createdAt: products[i].created,
+                      updatedAt: products[i].updated,
+                    },
+                  },
+                },
               },
-            },
-          },
-        },
-      };
-      result.data.push(dataObj);
+        })
+      }
     }
+
     return result;
   }
 }
