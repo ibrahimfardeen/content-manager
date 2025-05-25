e86111fb48a37adfbae7c5a68949f062799da00d
Premkumar <118468266+Premkumar708@users.noreply.github.com>
2025-05-25 12:56:59 +0530

    Update product.ts

--- app/models/product.ts
+++ app/models/product.ts
@@ -1,26 +1,31 @@
 import mongoose, { Schema } from "mongoose";
 
 const productSchema = new Schema({
+  category: {
+    type: String,
+    required: [true, "Please enter Category."],
+    trim: true,
+  },
   name: {
     type: String,
-    required: [true, "Please enter your name."],
+    required: [true, "Please enter Product ame."],
     trim: true,
   },
 
   price: {
     type: String,
-    required: [true, "Please enter your email."]
+    required: [true, "Please enter your email."],
   },
 
   description: {
     type: String,
-    required: [true, "Please enter your college name."],
+    required: [true, "Please enter product Desc."],
     trim: true,
   },
 
   photo: {
     type: String,
-    required: [true, "Paymentfile is required."],
+    required: [true, "Image is required"],
   },
 
   updated: {
@@ -30,9 +35,10 @@ const productSchema = new Schema({
   created: {
     type: Date,
     default: Date.now,
-  }
+  },
 });
 
-const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
