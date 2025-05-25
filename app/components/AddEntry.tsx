151bf4f99351048906d31e94808eef4be63c194e
Premkumar <118468266+Premkumar708@users.noreply.github.com>
2025-05-25 12:57:45 +0530

    Update AddEntry.tsx

--- app/components/AddEntry.tsx
+++ app/components/AddEntry.tsx
@@ -6,6 +6,7 @@ export default function AddEntry({ onclose }) {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [photo, setPhoto] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleCloseOnOverlayClick = (e) => {
     onclose();
@@ -27,112 +28,118 @@ export default function AddEntry({ onclose }) {
       console.log("err : " + err);
     }
   };
-
-  const handleSubmit = async (e) => { 
-    console.log('Submitted');
+  const handleSelectVal = (e) => {
+    setCategory(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    console.log("Submitted");
     const res = await fetch("../api/add", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify({
-        name,
-        price,
-        description,
-        photo,
-      }),
+      body: JSON.stringify({ category: { name, price, description, photo } }),
     });
     const { msg, success } = await res.json();
     console.log("msg : " + msg);
     console.log("success : " + success);
-
   };
 
   return (
     <>
-    <div
-  className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 backdrop-blur-sm rounded-lg z-50"
-  // onClick={handleCloseOnOverlayClick}
-    >
-
+      <div
+        className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 backdrop-blur-sm rounded-lg z-50"
+        // onClick={handleCloseOnOverlayClick}
+      >
         <div className="fixed flex-col top-0 left-0 w-full h-full flex items-center justify-center overflow-hidden z-50">
-          
-      <div className="mt-0" >
-        <button onClick={handleCloseOnOverlayClick}>Click here to close</button>
-      </div>  
-        <div className="w-[500px] h-[550px] bg-white overflow-y-auto p-8 popup-card-content relative">
-          <form
-            // action={handleSubmit}
-            onSubmit={handleSubmit}
-            className="py-4 mt-1 flex flex-col gap-5 bg-[white] overflow-auto relative"
-          >
-            <div>
-              {/* <label htmlFor="name">Name</label> */}
-              <input
-                className="border-2 border-gray-300 rounded-md w-full p-2"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                type="text"
-                id="name"
-                placeholder="Name"
-              />
-            </div>
-
-            <div>
-              {/* <label htmlFor="price">Price</label> */}
-              <input
-                className="border-2 border-gray-300 rounded-md p-2 w-full"
-                onChange={(e) => setPrice(e.target.value)}
-                value={price}
-                type="text"
-                id="price"
-                placeholder="Price"
-              />
-            </div>
-            <div>
-              {/* <label htmlFor="description">Description</label> */}
-              <input
-                className="border-2 border-gray-300 rounded-md p-2 w-full "
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-                type="text"
-                id="description"
-                placeholder="Description"
-              />
-            </div>
-            <div className="flex flex-col justify-center">
-              <label
-                htmlFor="file"
-                className="flex justify-center mb-2.5 bg-black px-4 py-2 text-white cursor-pointer mt-5 p-2 rounded-[0.5rem]"
-              >
-                Photo
-              </label>
-              <input
-                className="hidden"
-                onChange={(e) => convertTobase64(e)}
-                type="file"
-                id="file"
-                accept="image/*"
-              />
-            </div>
-            <div>
-              {/* <label htmlFor="description">Description</label> */}
-              <img src={photo} alt="Upload Photo"/>
-            </div>
-            <div>
-              {/* <label htmlFor="description">Description</label> */}
-              <button
-                className="border-2 border-gray-300 rounded-md p-2 w-full"
-                type="submit"
-              >
-                SUBMIT
-              </button>
-            </div>
-
-          </form>
-        </div>
+          <div className="mt-0">
+            <button onClick={handleCloseOnOverlayClick}>
+              Click here to close
+            </button>
           </div>
-    </div>
+          <div className="w-[500px] h-[550px] bg-white overflow-y-auto p-8 popup-card-content relative">
+            <form
+              // action={handleSubmit}
+              onSubmit={handleSubmit}
+              className="py-4 mt-1 flex flex-col gap-5 bg-[white] overflow-auto relative"
+            >
+              <div>
+                <select
+                  className="border-2 border-gray-300 rounded-md w-full p-2"
+                  onChange={handleSelectVal}
+                >
+                  <option>Cement</option>
+                  <option>Steel</option>
+                  <option>Brick</option>
+                  <option>Sand</option>
+                </select>
+              </div>
+              <div>
+                {/* <label htmlFor="name">Name</label> */}
+                <input
+                  className="border-2 border-gray-300 rounded-md w-full p-2"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                  id="name"
+                  placeholder="Name"
+                />
+              </div>
+
+              <div>
+                {/* <label htmlFor="price">Price</label> */}
+                <input
+                  className="border-2 border-gray-300 rounded-md p-2 w-full"
+                  onChange={(e) => setPrice(e.target.value)}
+                  value={price}
+                  type="text"
+                  id="price"
+                  placeholder="Price"
+                />
+              </div>
+              <div>
+                {/* <label htmlFor="description">Description</label> */}
+                <input
+                  className="border-2 border-gray-300 rounded-md p-2 w-full "
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                  type="text"
+                  id="description"
+                  placeholder="Description"
+                />
+              </div>
+              <div className="flex flex-col justify-center">
+                <label
+                  htmlFor="file"
+                  className="flex justify-center mb-2.5 bg-black px-4 py-2 text-white cursor-pointer mt-5 p-2 rounded-[0.5rem]"
+                >
+                  Photo
+                </label>
+                <input
+                  className="hidden"
+                  onChange={(e) => convertTobase64(e)}
+                  type="file"
+                  id="file"
+                  accept="image/*"
+                />
+              </div>
+              <div>
+                {/* <label htmlFor="description">Description</label> */}
+                <img src={photo} alt="Upload Photo" />
+              </div>
+              <div>
+                {/* <label htmlFor="description">Description</label> */}
+                <button
+                  className="border-2 border-gray-300 rounded-md p-2 w-full"
+                  type="submit"
+                >
+                  SUBMIT
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
